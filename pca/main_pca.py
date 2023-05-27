8fc6bea3e65538cf1799a23e8fe03b51819d628f
Alexandra Mikhailova <mikhaylova.alexandra.a@gmail.com>
2023-05-27 14:29:07 +0100

    Commit experiment - PCA transformation of memory maps (noise)

--- pca/main_pca.py
+++ pca/main_pca.py
@@ -178,9 +178,107 @@ def mnist17_01b_flip():
         f.write(str(dense_nn.model.evaluate(test_x, test_y)))
 
 
+def mnist17_01c_noise():
+    parent_dir = 'mnist17_01c'
+    experiment_dir = parent_dir
+
+    os.makedirs(experiment_dir, exist_ok=True)
+
+    train_x, train_y, test_x, test_y, train_batch_id = utils_mnist.basic_train_test()
+    # [(Gaussian noise STD, [(start of batch, end of batch - exclusive)])]
+    noise_settings = [
+        (0.3, [(10 * BATCH_SIZE, 15 * BATCH_SIZE), (66 * BATCH_SIZE, 71 * BATCH_SIZE)]),
+        (0.5, [(30 * BATCH_SIZE, 35 * BATCH_SIZE), (46 * BATCH_SIZE, 51 * BATCH_SIZE)]),
+        (0.7, [(39 * BATCH_SIZE, 42 * BATCH_SIZE)]),
+    ]
+    for noise_std, poisoned_batch_settings in noise_settings:
+        for start_idx, end_idx in poisoned_batch_settings:
+            train_x = utils_common._add_noise(train_x, start_idx, end_idx, noise_std, experiment_dir, plot=True)
+
+    poisoned_batch_mask_01 = np.zeros(shape=(NUM_TRAIN_BATCHES,), dtype=np.bool_)
+    poisoned_batch_mask_025 = np.zeros(shape=(NUM_TRAIN_BATCHES,), dtype=np.bool_)
+    poisoned_batch_mask_05 = np.zeros(shape=(NUM_TRAIN_BATCHES,), dtype=np.bool_)
+    poisoned_batch_mask_01[10:15] = True
+    poisoned_batch_mask_01[66:71] = True
+    poisoned_batch_mask_025[30:35] = True
+    poisoned_batch_mask_025[46:51] = True
+    poisoned_batch_mask_05[39:42] = True
+    poisoned_batch_mask = poisoned_batch_mask_01 + poisoned_batch_mask_025 + poisoned_batch_mask_05
+    assert sum(poisoned_batch_mask) == 23  # 23 poisoned batches in total.
+
+    dense_nn = dense.DenseNN(parent_dir=experiment_dir,
+                             name=parent_dir,
+                             input_dim=INPUT_DIM, h1_dim=H1_DIM, h2_dim=H2_DIM,
+                             classes=(1, 7), batch_size=BATCH_SIZE,
+                             mmap_normalise=False)
+    model_dir = dense_nn.model_dir
+    print('Model dir:', model_dir)
+
+    true_cluster_labels = [~poisoned_batch_mask, poisoned_batch_mask_01, poisoned_batch_mask_025, poisoned_batch_mask_05]
+    true_cluster_names = ['Unmodified', r'$\sigma = 0.3', r'$\sigma = 0.5', r'$\sigma = 0.7']
+    true_cluster_colours = ['blue', 'red', 'green', 'magenta']
+
+    pred_cluster_labels_by_epoch = []  # Just for 2 clusters.
+    cluster_names = ('Cluster 1', 'Cluster 2')
+    cluster_colours = ('blue', 'red')
+    mmap_pca_by_epoch = []
+
+    for epoch in range(NUM_EPOCHS):
+        current_epoch = len(dense_nn.epoch_mmaps) + 1
+
+        dense_nn.fit(train_x, train_y,
+                     validation_data=(test_x, test_y), batch_size=BATCH_SIZE, num_epochs=1, continue_training=True)
+
+        # Model is loaded during fit(). Start from scratch every time.
+        assert len(dense_nn.epoch_mmaps) == epoch + 1
+
+        mmap, _, _ = dense_nn.epoch_mmaps[-1]  # mmap mush already be demeaned here.
+        assert mmap.shape[0] == NUM_TRAIN_BATCHES
+
+        pca = PCA(n_components=3)
+        mmap_pca = pca.fit_transform(mmap)
+        mmap_pca_by_epoch.append(mmap_pca)
+        explained_var = [round(val, 5) for val in pca.explained_variance_]
+        singular_values = [round(val, 5) for val in pca.singular_values_]
+
+        # Try to cluster into 2 clusters.
+        kmeans = KMeans(n_clusters=2, random_state=0).fit(mmap_pca)
+        pred_cluster_masks = [kmeans.labels_ == i for i in range(2)]
+        pred_cluster_labels_by_epoch.append(kmeans.labels_)
+
+        utils_common.plot_mmap_pca(mmap_pca, current_epoch, pred_cluster_masks, cluster_names, cluster_colours,
+                                   explained_var, singular_values, 0, 1, model_dir, is_true=False)
+        utils_common.plot_mmap_pca(mmap_pca, current_epoch, true_cluster_labels,
+                                   true_cluster_names, true_cluster_colours,
+                                   explained_var, singular_values, 0, 1, model_dir, is_true=True)
+
+        utils_common.plot_mmap_pca(mmap_pca, current_epoch, pred_cluster_masks, cluster_names, cluster_colours,
+                                   explained_var, singular_values, 0, 2, model_dir, is_true=False)
+        utils_common.plot_mmap_pca(mmap_pca, current_epoch, true_cluster_labels,
+                                   true_cluster_names, true_cluster_colours,
+                                   explained_var, singular_values, 0, 2, model_dir, is_true=True)
+
+        utils_common.plot_mmap_pca(mmap_pca, current_epoch, pred_cluster_masks, cluster_names, cluster_colours,
+                                   explained_var, singular_values, 1, 2, model_dir, is_true=False)
+        utils_common.plot_mmap_pca(mmap_pca, current_epoch, true_cluster_labels,
+                                   true_cluster_names, true_cluster_colours,
+                                   explained_var, singular_values, 1, 2, model_dir, is_true=True)
+
+    utils_common.cluster_analysis_2(mmap_pca_by_epoch, pred_cluster_labels_by_epoch, cluster_names, cluster_colours,
+                                    model_dir, NUM_EPOCHS)
+
+    # Save the final model to perform model-based poisoning later.
+    dense_nn.model.save(experiment_dir + '/MNIST_for_poison.h5')
+
+    # Evaluate quality of the final model.
+    with open(experiment_dir + '/eval.txt', 'w') as f:
+        f.write(str(dense_nn.model.evaluate(test_x, test_y)))
+
+
 def main():
     # mnist17_01a()
-    mnist17_01b_flip()
+    # mnist17_01b_flip()
+    mnist17_01c_noise()
     pass
 
 
--- pca/utils_common.py
+++ pca/utils_common.py
@@ -2,7 +2,11 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+from numpy.random import normal
 from sklearn.metrics import silhouette_score
+
+from constants_mnist_pca import *
 
 
 def flip_labels(orig_labels, start_idx, end_idx, flip_proba, batch_size,
@@ -19,6 +23,50 @@ def flip_labels(orig_labels, start_idx, end_idx, flip_proba, batch_size,
     labels[idx, :] = 1 - old_labels
 
     return labels
+
+
+def noise_poisoning(train_x, train_y, plot_dir=None, plot=False):
+    if plot:
+        assert plot_dir is not None
+
+    # [(Gaussian noise STD, [(start of batch, end of batch - exclusive)])]
+    noise_settings = [
+        (0.3, [(10 * BATCH_SIZE, 15 * BATCH_SIZE), (66 * BATCH_SIZE, 71 * BATCH_SIZE)]),
+        (0.5, [(30 * BATCH_SIZE, 35 * BATCH_SIZE), (46 * BATCH_SIZE, 51 * BATCH_SIZE)]),
+        (0.7, [(39 * BATCH_SIZE, 42 * BATCH_SIZE)]),
+    ]
+    for noise_std, poisoned_batch_settings in noise_settings:
+        for start_idx, end_idx in poisoned_batch_settings:
+            train_x = _add_noise(train_x, start_idx, end_idx, noise_std, plot_dir, plot=plot)
+    true_batch_labels = np.zeros(shape=(NUM_TRAIN_BATCHES,))
+    true_batch_labels[10:15] = 1
+    true_batch_labels[66:71] = 1
+    true_batch_labels[30:35] = 2
+    true_batch_labels[46:51] = 2
+    true_batch_labels[39:42] = 3
+
+    return train_x, train_y, true_batch_labels
+
+
+def _add_noise(x, start_idx, end_idx, noise_std, iter_dir, plot=True):
+    """
+    Noise poisoning: add Gaussian noise.
+    """
+
+    noise_sample = normal(0., noise_std, size=x[start_idx:end_idx].shape)
+    x[start_idx:end_idx] += noise_sample
+    x = np.clip(x, a_min=0, a_max=1)
+
+    if plot:
+        f, ax = plt.subplots(2, 5, figsize=(10, 5))
+        ax = ax.flatten()
+        for i in range(10):
+            idx = start_idx + i
+            ax[i].imshow(x[idx].reshape(28, 28))
+        plt.savefig(os.path.join(iter_dir, 'noise{}.png'.format(noise_std)), dpi=150)
+        plt.gcf().clear()
+
+    return x
 
 
 def merge(x, y, start_idx, end_idx, rate, model_dir):
